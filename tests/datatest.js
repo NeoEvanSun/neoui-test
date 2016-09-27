e514bad2992c981043ad2b1bd2607c66db2bc7df
NeoEvanSun <olesa@foxmail.com>
2016-09-27 09:39:10 +0800

    add the grunt plugin

--- tests/datatest.js
+++ tests/datatest.js
@@ -8,13 +8,29 @@ describe('The test case for data',function(){
   var driver;
   this.timeout(20000);
   before(function(){
-    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
   })
   after(function(){
     driver.quit();
   })
 
-  it('go to the data page',function(){
+  it('tesst the data page for firefox',function(){
+    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+    return driver.get('http://localhost:8090/widgets/dataTable.html').then(function(){
+      var td = driver.findElement(By.css('table.u-table>tbody>tr:first-child>td:nth-child(2)'));
+      return td.getText();
+    }).then(function(text){
+      expect(text).to.equal('用友2016电脑采购对账1');
+    }).then(function(){
+      driver.findElement(By.css('div.u-pagination li:nth-child(3)')).click();
+      var td = driver.findElement(By.css('table.u-table>tbody>tr:first-child>td:nth-child(2)'));
+      return td.getText();
+    }).then(function(text){
+      expect(text).to.equal('用友2016电脑采购对账11');
+    });
+  })
+
+  it('tesst the data page for chrome',function(){
+    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
     return driver.get('http://localhost:8090/widgets/dataTable.html').then(function(){
       var td = driver.findElement(By.css('table.u-table>tbody>tr:first-child>td:nth-child(2)'));
       return td.getText();
